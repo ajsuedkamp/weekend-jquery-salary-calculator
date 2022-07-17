88dfcbf2055555305f57d0fd57502b8298080a91
Alex Suedkamp <ajsuedkamp@gmail.com>
2022-07-17 15:23:31 -0500

    get the values of the input fields

--- client.js
+++ client.js
@@ -6,9 +6,15 @@ function readyNow() {
     console.log('ready now!');
     $( 'h1' ).text('Salary Calculator'); //make sure jquery is working correctly
     $( '#submit' ).on( 'click', addEmployee);
-}
+} // end readyNow
 
 
 function addEmployee() {
-    
+ let firstName = $( '#firstNameInput' ).val();
+ let lastName = $( '#lastNameInpute' ).val();
+ let employeeID =  $( '#employeeID' ).val();
+ let jobTitle = $( '#jobTitle' ).val();
+ let annualSalary = $( '#annualSalary' ).val();
+
+ console.log('add an employee', firstName);
 }
